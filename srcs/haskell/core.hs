311a9ca8c4c8fa78154d1d5f7b0b7ee7ba8f6b0f
KukuruzaAndrey <andreyku6@gmail.com>
2022-07-18 00:52:05 +0300

    st

--- srcs/haskell/core.hs
+++ srcs/haskell/core.hs
@@ -1,2 +1,41 @@
-main :: IO ()
-main = putStrLn $ "Hello, tetris"
+import System.Environment   
+main :: IO () 
+main = do
+  args <- getArgs
+  let state = parseState args
+      nextState = update state
+  putStrLn $ printState nextState
+  putStrLn $ render nextState
+
+data Move = Down | Left | Right | Rotate_C | Rotate_C_C | Drop
+type Board = [[Int]]
+data State = State
+             {
+               move :: Move,
+               board :: Board,
+               figIndex :: Int,
+               rotateIndex :: Int,
+               color :: Int,
+               offsetX :: Int,
+               offsetY :: Int,
+               nextFigIndex :: Int,
+               nextFigColor :: Int,
+               score :: Int
+             }
+
+parseBoard :: String -> [[Int]]
+parseBoard = 
+parseState :: [String] -> State
+parseState args = State
+  (toEnum . read $ args !! 0)
+  (parseBoard $ args !! 1)
+  
+
+update :: State -> State
+update s = State Rotate_C
+
+printState :: State -> String
+printState s = "state"
+
+render :: State -> String
+render s = "tetris"
